6bc4b4ad0908d5c644ca9082d829f8b54a077deb
Edgar Jared <mejiaapolinaredgar@gmail.com>
2023-05-12 14:48:24 -0600

    se agrego hola mundo

---
+++ hola-mundo/src/HolaMundo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hola.mundo;
+
+/**
+ *
+ * @author edgar jared
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("hola mundo!");
+    }
+}
